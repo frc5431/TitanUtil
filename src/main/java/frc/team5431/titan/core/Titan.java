4d6d67436e75b06ef1382a3b9f17a424b94086ff
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-08-01 17:56:34 -0500

    Lidar

--- src/main/java/frc/team5431/titan/core/Titan.java
+++ src/main/java/frc/team5431/titan/core/Titan.java
@@ -120,36 +120,12 @@ public final class Titan{
 		}
 	}
 
-	public static class Lidar extends Counter{
-		private int calibrationOffset = 0;
-		
+	public static class Lidar extends TitanLidar{
 		public Lidar(final int source) {
 			this(new DigitalInput(source));
 		}
-		
-		public Lidar(final DigitalSource source) {
+		public Lidar(DigitalSource source) {
 			super(source);
-			setMaxPeriod(1.0);
-			setSemiPeriodMode(true);
-			setSamplesToAverage(100);
-			reset();
-		}
-		
-		public int getCalibrationOffset() {
-			return calibrationOffset;
-		}
-
-		public void setCalibrationOffset(final int calibrationOffset) {
-			this.calibrationOffset = calibrationOffset;
-		}
-		/*
-		 * @return distance in cm*/
-		public double getDistance() {
-			if(get() < 1) {
-				return -1;
-			}
-			
-			return ((getPeriod() * 1000000.0 / 10.0) + calibrationOffset) * 0.39370079;
 		}
 	}
 
