82278e73e79bb8e3d6bc905b9d76c0139f3f829f
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-08-02 16:28:27 -0500

    Expanded Solenoid

--- src/main/java/frc/team5431/titan/core/Solenoid.java
+++ src/main/java/frc/team5431/titan/core/Solenoid.java
@@ -1,43 +1,7 @@
 package frc.team5431.titan.core;
 
 public class Solenoid{
-    public static class Single extends edu.wpi.first.wpilibj.Solenoid{
-        private boolean currentState = false;
+  
 
-    public Single(final int channel){
-        super(channel);
-    }
 
-    public Single(final int moduleNumber, final int channel) {
-        super(moduleNumber, channel);
-    }
-
-    @Override
-    public void set(final boolean newState){
-        if(currentState != newState){
-            currentState = newState;
-            super.set(newState);
-        }
-    }
-    }
-
-    public static class Double extends edu.wpi.first.wpilibj.DoubleSolenoid {
-        private Value currentState = Value.kOff;
-
-        public Double(final int forwardChannel, final int reverseChannel){
-            super(forwardChannel, reverseChannel);
-        }
-    
-        public Double(final int moduleNumber, final int forwardChannel, final int reverseChannel){
-            super(moduleNumber, forwardChannel, reverseChannel);
-        }
-    
-        @Override
-        public void set(final Value newState){
-            if(currentState != newState){
-                currentState = newState;
-                super.set(newState);
-            }
-        }
-    }
 }
