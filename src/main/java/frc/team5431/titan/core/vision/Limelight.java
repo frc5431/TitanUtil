f0f7c93f1b18356bfbec9a4836d969b6e794eeab
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-09-16 12:05:46 -0500

    limelight data

---
+++ src/main/java/frc/team5431/titan/core/vision/Limelight.java
@@ -0,0 +1,35 @@
+package frc.team5431.titan.core.vision;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Limelight {
+
+    private NetworkTable table;
+
+    public Limelight(final String dev) {
+        table = NetworkTableInstance.getDefault().getTable(dev);
+    }
+
+    private final Number get(String val) {
+        return table.getEntry(val).getNumber(0.0);
+    }
+
+    public class basic {
+        public final double getX() {
+            return get("tx").doubleValue();
+        }
+
+        public final double getY() {
+            return get("ty").doubleValue();
+        }
+
+        public final double getArea() {
+            return get("ta").doubleValue();
+        }
+
+        public final int getValid() {
+            return get("tv").intValue();
+        }
+    }
+}
