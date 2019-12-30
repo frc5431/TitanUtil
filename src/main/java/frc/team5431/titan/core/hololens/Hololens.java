afb9130f921e642ab3411640cba22b5a9276804e
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-12-30 15:05:53 -0600

    cleaned um hololens code

--- src/main/java/frc/team5431/titan/core/hololens/Hololens.java
+++ src/main/java/frc/team5431/titan/core/hololens/Hololens.java
@@ -3,20 +3,35 @@ package frc.team5431.titan.core.hololens;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
-import frc.team5431.titan.core.robot.Component;
-import frc.team5431.titan.core.robot.TitanRobot;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Hololens {
 
-    private NetworkTable hololens_table;
-    private String custommsg = "";
+    private NetworkTable m_Table;
+    private String m_Message;
+    private Lock m_Mutex;
 
     public Hololens() {
-        hololens_table = NetworkTableInstance.getDefault().getTable("hololens");
+        m_Table = NetworkTableInstance.getDefault().getTable("hololens");
+        m_Message = new String();
+        m_Mutex = new ReentrantLock(true);
     }
 
-    public void customMsg(final String msg) {
-        custommsg = msg;
+    public void setCustomMessage(final String msg) {
+        m_Mutex.lock();
+        m_Message = String.valueOf(msg);
+        m_Mutex.unlock();
+    }
+
+    public String getCustomMessage() {
+        String buffer;
+
+        m_Mutex.lock();
+        buffer = String.valueOf(m_Message);
+        m_Mutex.unlock();
+
+        return buffer;
     }
 
     public void tick() {
@@ -25,7 +40,8 @@ public class Hololens {
         double time  = driverStation.getMatchTime();
         int matchNumber = driverStation.getMatchNumber();
 
-        hololens_table.getEntry("time").setDouble(time);
-        hololens_table.getEntry("custom").setString(custommsg);
+        m_Table.getEntry("time").setDouble(time);
+        m_Table.getEntry("match_number").setDouble(matchNumber);
+        m_Table.getEntry("custom").setString(getCustomMessage());
     }
 }
