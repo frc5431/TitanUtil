1aa3a386f3f7e567ec9101a895854c308162419b
Ryan Hirasaki <ryan@DESKTOP-0HDTE7C.fios-router.home>
2020-02-08 18:08:39 -0600

    Remove extra value from string

--- src/main/java/frc/team5431/titan/mimic/Stepper.java
+++ src/main/java/frc/team5431/titan/mimic/Stepper.java
@@ -8,7 +8,7 @@ import frc.team5431.titan.core.misc.Logger;
  */
 public class Stepper {
     public static final String mimicFile = "/media/sda1/%s.mimic";
-    public static final String formatString = "%.2f,%.2f,%.2f,%.4f,%.4f,%d,%d\n"; //LEFT ENCODER, RIGHT ENCODER, GYRO ANGLE, LEFT POWER, RIGHT POWER, HOME
+    public static final String formatString = "%.2f,%.2f,%.2f,%.4f,%.4f,%d\n"; //LEFT ENCODER, RIGHT ENCODER, GYRO ANGLE, LEFT POWER, RIGHT POWER, HOME
     public double leftDistance, rightDistance, angle, leftPower, rightPower;
     public boolean isHome;
 
