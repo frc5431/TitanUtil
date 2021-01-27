fd700fc21792b33a93da7db68ea98b72d52e688e
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-01-27 16:09:22 -0600

    Constructor for DriveConfig

--- src/main/java/frc/team5431/titan/pathweaver/DriveConfig.java
+++ src/main/java/frc/team5431/titan/pathweaver/DriveConfig.java
@@ -1,15 +1,30 @@
 public class DriveConfig {
-    public double ksVolts;
-    public double kvVoltSecondsPerMeter;
-    public double kaVoltSecondsSquaredPerMeter;
-
-    public double kTrackwidthMeters;
-
-    public double kMaxSpeedMetersPerSecond;
-    public double kMaxAccelerationMetersPerSecondSquared;
-
-    public double kRamseteB;
-    public double kRamseteZeta;
-
-    public double kPDriveVel;
+    public DriveConfig(double sVolts.
+                        double vVoltSPM,
+                        double aVoltSSPM,
+                        double trackwidth,
+                        double maxSpeedMPS,
+                        double maxAccelMPSS,
+                        double ramseteb,
+                        double ramsetezeta,
+                        double pDriveVel) {
+        this.ksVolts = sVolts;
+        this.kvVoltSecondsPerMeter = vVoltSPM;
+        this.kaVoltSecondsSquaredPerMeter = aVoltSSPM;
+        this.kTrackwidthMeters = trackwidth;
+        this.kMaxSpeedMetersPerSecond = maxSpeedMPS;
+        this.kMaxAccelerationMetersPerSecondSquared = maxAccelMPSS;
+        this.kRamseteB = ramseteb;
+        this.kRamseteZeta = ramsetezeta;
+        this.kPDriveVel = pDriveVel;
+    }
+    public final double ksVolts;
+    public final double kvVoltSecondsPerMeter;
+    public final double kaVoltSecondsSquaredPerMeter;
+    public final double kTrackwidthMeters;
+    public final double kMaxSpeedMetersPerSecond;
+    public final double kMaxAccelerationMetersPerSecondSquared;
+    public final double kRamseteB;
+    public final double kRamseteZeta;
+    public final double kPDriveVel;
 }
