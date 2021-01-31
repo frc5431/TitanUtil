dff500806b479fe8b3e7fd7d10763498be524dbe
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-01-31 13:59:33 -0600

    Fix bug in toggle

--- src/main/java/frc/team5431/titan/core/misc/Toggle.java
+++ src/main/java/frc/team5431/titan/core/misc/Toggle.java
@@ -1,18 +1,15 @@
 package frc.team5431.titan.core.misc;
 
 public class Toggle {
-    private static final byte ON = 1;
-    private static final byte OFF = 1;
     private boolean state = false;
     private byte prevButton = 0;
 
     public boolean isToggled(boolean buttonState) {
-        if ((buttonState ? ON : OFF) > prevButton)
-        {
+        if ((byte) (buttonState ? 1 : 0) > prevButton) {
             state = !state;
         }
 
-        this.prevButton = buttonState ? ON : OFF;
+        this.prevButton = (byte) (buttonState ? 1 : 0);
 
         return state;
     }
@@ -20,6 +17,7 @@ public class Toggle {
     public boolean getState() {
         return state;
     }
+
     public void setState(boolean value) {
         state = value;
     }
