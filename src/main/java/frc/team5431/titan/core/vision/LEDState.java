de80a07879ba1aa0a2a455c4d7cb773d6789cab8
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-01-31 18:29:11 -0600

    Simplify Limelight enums

--- src/main/java/frc/team5431/titan/core/vision/LEDState.java
+++ src/main/java/frc/team5431/titan/core/vision/LEDState.java
@@ -4,24 +4,5 @@ package frc.team5431.titan.core.vision;
  * @author Ryan Hirasaki
  */
 public enum LEDState {
-    // Values Gathered from limelight documentation
-    DEFAULT(0), OFF(1), BLINK(2), ON(3);
-
-    private final int id;
-
-    private LEDState(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public static LEDState getStateFromInt(int value) {
-        for (LEDState x : LEDState.values()) {
-            if (x.getId() == value)
-                return x;
-        }
-        return null;
-    }
+    DEFAULT, OFF, BLINK, ON;
 }
