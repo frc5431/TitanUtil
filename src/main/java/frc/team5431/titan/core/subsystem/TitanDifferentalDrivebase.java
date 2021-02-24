835d4aa76d0fe522c89887c8069c5c3805c066f3
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-24 12:19:58 -0600

    remove inversion

--- src/main/java/frc/team5431/titan/core/subsystem/TitanDifferentalDrivebase.java
+++ src/main/java/frc/team5431/titan/core/subsystem/TitanDifferentalDrivebase.java
@@ -2,8 +2,10 @@ package frc.team5431.titan.core.subsystem;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import frc.team5431.titan.core.misc.Logger;
 
 public abstract class TitanDifferentalDrivebase extends TitanBaseDrivebase<DifferentialDriveWheelSpeeds> {
+    private static final String PATH = "frc.team5431.titan.core.subsystem.TitanDifferentalDrivebase";
     private SpeedController left;
     private SpeedController right;
     private final double maxTurnValue;
@@ -16,12 +18,16 @@ public abstract class TitanDifferentalDrivebase extends TitanBaseDrivebase<Diffe
         this.left = (SpeedController) left;
         this.right = (SpeedController) right;
         this.maxTurnValue = maxTurnValue;
+
+        if (this.left.getInverted() == this.right.getInverted()) {
+            Logger.l("WARN: %s: %s", PATH, "Left and Right inversion setting is the same");
+        }
     }
 
     @Override
     public final void driveTank(double left, double right) {
         this.left.set(left);
-        this.right.set(-right);
+        this.right.set(right);
     }
 
     @Override
@@ -33,6 +39,6 @@ public abstract class TitanDifferentalDrivebase extends TitanBaseDrivebase<Diffe
     @Override
     public final void driveArcade(double power, double turn) {
         left.set(power + turn * maxTurnValue);
-        right.set(power - turn * maxTurnValue);
+        right.set(power + turn * maxTurnValue);
     }
 }
