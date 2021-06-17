47d539ae2f6d4dda681ce99c1be67ea7ecbf77fb
Colin Wong <cwongmath@gmail.com>
2021-06-17 17:55:01 -0500

    Add Calc.map, .misc docs

--- src/main/java/frc/team5431/titan/core/misc/Logger.java
+++ src/main/java/frc/team5431/titan/core/misc/Logger.java
@@ -1,21 +1,36 @@
 package frc.team5431.titan.core.misc;
 
 public class Logger {
-    public static boolean DEBUG = false;
+	public static boolean DEBUG = false;
 
-	/* Log information */
+	/**
+	 * Logs information
+	 * 
+	 * @param base base string, use %s for objects
+	 * @param a    objects to replace %s in order
+	 */
 	public static void l(String base, Object... a) {
 		if (DEBUG)
 			System.out.println(String.format(base, a));
 	}
 
-	/* Log error */
+	/**
+	 * Logs errors
+	 * 
+	 * @param base base string, use %s for objects
+	 * @param a    objects to replace %s in order
+	 */
 	public static void e(String base, Object... a) {
 		if (DEBUG)
 			System.err.println(String.format(base, a));
 	}
 
-	/* Exception error */
+	/**
+	 * Logs exceptions
+	 * 
+	 * @param namespace exception namespace (before the colon)
+	 * @param e         exception to log
+	 */
 	public static void ee(String namespace, Exception e) {
 		if (DEBUG)
 			e("%s: %s", namespace, e.getMessage());
--- src/main/java/frc/team5431/titan/core/misc/Toggle.java
+++ src/main/java/frc/team5431/titan/core/misc/Toggle.java
@@ -1,13 +1,34 @@
 package frc.team5431.titan.core.misc;
 
+/**
+ * Helper class to store button states
+ */
 public class Toggle {
-    private boolean curr_state = false;
-    private boolean prev_state = false;
+    private boolean toggleState;
+    private boolean prevButtonState = false;
 
     /**
+     * Creates a new toggle with a default state of false.
+     */
+    public Toggle() {
+        this(false);
+    }
+
+    /**
+     * Creates a new toggle with the specified default state.
+     * 
+     * @param defaultState the default state of the toggle
+     */
+    public Toggle(boolean defaultState) {
+        this.toggleState = defaultState;
+    }
+
+    /**
+     * Whether the toggle is active
+     * 
      * @deprecated use update() as it is more descriptive
-     * @param buttonState button status
-     * @return current state
+     * @param buttonState raw button status
+     * @return the toggle state after raw data is received
      */
     @Deprecated(forRemoval = true)
     public boolean isToggled(boolean buttonState) {
@@ -18,28 +39,31 @@ public class Toggle {
     /**
      * Updates internal state for if toggle has changed
      * 
-     * @param pressed button status
+     * @param raw button data
      */
     public void update(boolean pressed) {
         // "^=true" will toggle bool, "^=false" will not.
         // "pressed && !prev_state" checks whether state needs to be changed.
         // Using this over if/else logic allows the JVM to better optimize the logic to
         // CPU instructions.
-        curr_state ^= pressed && !prev_state;
+        toggleState ^= pressed && !prevButtonState;
 
-        this.prev_state = pressed;
+        this.prevButtonState = pressed;
     }
 
+    /**
+     * @return the toggle state
+     */
     public boolean getState() {
-        return curr_state;
+        return toggleState;
     }
 
     /**
      * Use with caution, can break internal logic
      * 
-     * @param value new state value
+     * @param value the new state value
      */
     public void setState(boolean value) {
-        curr_state = value;
+        toggleState = value;
     }
 }
